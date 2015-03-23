166d6d7c14365a84752e1098d2b6316c9ee20646
gsick <gamaliel.sick@yahoo.fr>
2015-03-23 08:57:03 +0100

    initial commit

---
+++ package.lua
@@ -0,0 +1,10 @@
+return {
+  name = "gsick/clocktime",
+  version = "0.0.1",
+  description = "C clocktime wrapped in lua ffi and packaged for lit",
+  author = "Gamaliel Sick",
+  homepage = "https://github.com/gsick/lit-clocktime",
+  files = {
+    "init.lua"
+  }
+}
